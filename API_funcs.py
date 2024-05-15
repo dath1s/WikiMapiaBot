913939a421a31dd3ddcdf82972d777e1bb40464c
dmitry <kovtunov.da@students.dvfu.ru>
2024-05-15 14:44:07 +1000

    update search method

--- API_funcs.py
+++ API_funcs.py
@@ -1,58 +1,47 @@
-from wikimapia_api import API
-import numpy as np
-from math import floor
+import requests
+import json
 import pandas as pd
-
-# Настройки API
-API.config = \
-    {
-        'key': '0838653D-6414A31B-CC5BB66E-6F261C45-B1DEDAB0-59F1237E-0ACEDCBE-FD5E8D3A',
-        'delay': 500,
-        'language': 'ru'
-    }
 
 
 # Функция для формирования CSV-файла
 def get_data(lat1, lon1, lat2, lon2) -> int:
-    # Сортировка координат для формирования максимально и минимальной пары
+    key = 'example',
+    language = 'ru'
+    format = 'json'
+    function = 'box'
+
     latMin, latMax = sorted([lat1, lat2])
     lonMin, lonMax = sorted([lon1, lon2])
 
-    # Множетсво уникальных мест на карте
-    placesSet = set()
-
-    # Деление области на более мелкие полигоны для поиска
-    lonLinSpace = np.linspace(lonMin, lonMax, num=floor(lonMax * 1e3) - floor(lonMin * 1e3))
-    latLinSpace = np.linspace(latMin, latMax, num=floor(latMax * 1e3) - floor(latMin * 1e3))
-
-    # Поиск ближайших объектов в более мелких областях
-    for lon in lonLinSpace:
-        for lat in latLinSpace:
-            places = API.places.nearest(lon, lat)
-
-            for obj in places:
-                # Проверка на удовлетворение условию по координатам
-                if lonMin <= obj['location']['lon'] <= lonMax and latMin <= obj['location']['lat'] <= latMax:
-                    placesSet.add(str(obj))
-
-    # Формировние CSV-файла
+    page = 1
     ID, TITLE, URL, LOC = [[] for _ in range(4)]
 
-    for obj in placesSet:
-        objDict = eval(obj)
+    while True:
+        resp = f'https://api.wikimapia.org/?key={key[0]}&function={function}&format={format}&language={language}&lon_min={lonMin}&lat_min={latMin}&lon_max={lonMax}&lat_max={latMax}&count=100&page={page}'
 
-        ID.append(objDict['id'])
-        TITLE.append(objDict['title'])
-        URL.append(objDict['url'])
-        LOC.append(objDict['location'])
+        response = requests.get(resp)
+        resJSON = json.loads(response.text)
+        objects = resJSON["folder"]
 
-    dataFrame = pd.DataFrame(data={
-        'id': ID,
-        'Название': TITLE,
-        'Ссылка': URL,
-        'Расположение': LOC
-    })
+        if objects:
+            for obj in objects:
+                ID.append(obj['id'])
+                TITLE.append(obj['name'])
+                URL.append(obj['url'])
+                LOC.append(obj['location'])
+            page += 1
+        else:
+            break
+
+    dataFrame = pd.DataFrame(
+        data={
+            'id': ID,
+            'Название': TITLE,
+            'Ссылка': URL,
+            'Расположение': LOC
+        }
+    )
 
     dataFrame.to_csv('output.csv', encoding='utf-8', index=False)
 
-    return len(placesSet)
+    return len(dataFrame)
